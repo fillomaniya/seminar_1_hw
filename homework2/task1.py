97a1b5dac1c33910c4a442f4d52dabdc0c23a774
Ilya <fillomaniya@mail.ru>
2023-11-08 17:31:01 +0300

    add hw2

---
+++ homework2/task1.py
@@ -0,0 +1,14 @@
+def multiplication_table(n):
+    for i in range(1, n + 1):
+        for j in range(1, 10):
+            print(f"{i} * {j} = {i * j}")
+        print('')
+
+
+def main():
+    n = int(input("Введите число n: "))
+    multiplication_table(n)
+
+
+if __name__ == "__main__":
+    main()
