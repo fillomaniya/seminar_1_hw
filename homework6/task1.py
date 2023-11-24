96ad7f53fe7a980983061ce0359b4c4678aa0138
Ilya <fillomaniya@mail.ru>
2023-11-24 20:52:19 +0300

    add hw6

---
+++ homework6/task1.py
@@ -0,0 +1,31 @@
+from typing import List
+
+def binary_search(arr: List[int], number: int) -> int:
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == number:
+            return mid
+        elif arr[mid] < number:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+def main():
+    arr = [1, 2, 7, 6, 11, 33, 45]
+
+    print(f"Исходный массив: {arr}")
+    number = int(input("Введите искомый элемент: "))
+    result = binary_search(arr, number)
+
+    if result == -1:
+        print("Искомого элемента нет в массиве")
+    else:
+        print(f"Искомый элемент: {number} найден по индексу: {result}")
+
+if __name__ == "__main__":
+    main()
